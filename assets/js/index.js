e87e3208ce5dea5eff68cb453d60d4ec89a56d87
zhong-heng <2500403588@qq.com>
2020-12-15 16:09:08 +0800

    out ok

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,17 +12,33 @@ function getAjax() {
         success: (res) => {
             console.log(res);
             if (res.status !== 0) {
-                return layer.msg(res.message);
+                // return layer.msg(res.message);
+                localStorage.removeItem("token");
+                location.href = '../../home/login.html'
             }
             let name = res.data.username || res.data.nickname
             $('.uname').text(name)
-            console.log(name[0].toUpperCase());
+            // console.log(name[0].toUpperCase());
             if (!res.data.user_pic) {
                 $('.text').text(name[0].toUpperCase())
+                $('.layui-nav-img').hide()
+
             } else {
                 $('.text').hide()
-                $('.layui-nav-img').show()
+                $('.layui-nav-img').attr('src', res.data.user_pic).show()
             }
         }
     })
 }
+// 退出
+$("#out").on("click", function () {
+    // console.log(111);
+    layer.confirm('请确认退出', {icon: 3, title:'提示'}, function(index){
+        //do something
+        
+        layer.close(index);
+        // 删除本地存储的token
+        localStorage.removeItem("token")
+        location.href= "../../home/login.html"
+      });
+})
